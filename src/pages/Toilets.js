299f926e095b3742c07058693c061302b9ef160f
becky1030 <pyh245@nyu.edu>
2023-04-25 02:51:02 -0400

    map update

--- src/pages/Toilets.js
+++ src/pages/Toilets.js
@@ -1,134 +1,140 @@
-import React from 'react';
-
-// import "mapbox-gl/dist/mapbox-gl-css";
-import Map, {Marker} from "react-map-gl";
-import "../styles/Toilets.css";
-import { useState } from "react";
+import React from "react";
+import Map, {Source, Layer, Marker} from 'react-map-gl';
+import nycWifi from "../assets/wifi.json";
 
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiYmVja3l5eXl5IiwiYSI6ImNsZWV2azM0bTBiN2k0NG12cnEybml0am0ifQ.pTk0bhJgKynBeJMf1r8N3A'; // Set your mapbox token here
 
 
-function App(){
-  // const [lng, setLng] = useState(-73.98451127670837);
-  // const [lat, setLat] = useState(40.7554292155312);
 
- 
+const layerStyle = {
+  id: 'point',
+  type: 'circle',
+  paint: {
+    'circle-radius': 10,
+    'circle-color': '#007cbf'
+  }
+};
 
 
+
+function Toilet() {
+  const [viewport, setViewport] = React.useState();
   return (
-      <div className="App">
-        <Map
-        mapboxAccessToken="pk.eyJ1IjoiYmVja3l5eXl5IiwiYSI6ImNsZWV2azM0bTBiN2k0NG12cnEybml0am0ifQ.pTk0bhJgKynBeJMf1r8N3A"
-        style={{
-
-              width:"90%",
-              height: "60%",
-              borderRadius:"15px",
-              border: '2px solid red',
-              top: "200px",
-              bottom: "0",
-              // backgroundColor: '#fff200'
-              // marginLeft: "80px",
-              // marginTop: "200px"
-                
-        }}
-        initialViewState={{
-                  longitude: -73.98451127670837,
-                  latitude: 40.7554292155312,
-                  projection: 'globe',
-                  zoom: 15,
-                  pitch: 60
-                }}
-        mapStyle="mapbox://styles/mapbox/streets-v12"
-        >
-<Marker 
-longitude={-73.98451127670837}
-latitude={40.7554292155312}
-        
-/>
+    <Map 
 
 
-</Map>
-</div>
-);
-}
+ mapboxAccessToken="pk.eyJ1IjoiYmVja3l5eXl5IiwiYSI6ImNsZWV2azM0bTBiN2k0NG12cnEybml0am0ifQ.pTk0bhJgKynBeJMf1r8N3A"
 
-export default App
+    style={{
+
+                    width:"90%",
+                    height: "60%",
+                    borderRadius:"15px",
+                    border: '2px solid red',
+                    top: "200px",
+                    bottom: "0",
+                    
+                      
+              }}
+    
+    
+    initialViewState={{
 
 
-// function Toilets() {
+      longitude: -73.98451127670837,
+      latitude: 40.7554292155312,
+      zoom: 15,
+      pitch: 60
+    }}
+            mapStyle="mapbox://styles/mapbox/streets-v12"
 
-//   return (
-//     <div className="toilet" style={{backgroundColor: '#fff200'}}>
-      
-   
-//     <Map
-//       initialViewState={{
-//         longitude: -122.4,
-//         latitude: 37.8,
-//         zoom: 14
-//       }}
-//       style={{width: 600, height: 400}}
-//       mapStyle="mapbox://styles/mapbox/streets-v12"
-//       mapboxAccessToken={MAPBOX_TOKEN}
-//       >
-
-         {/* <Source id="my-data" type="geojson" data={'./assets/wifi.geojson'}>
-        <Layer {...layerStyle} /> 
+    >
+      <Source id="my-data" type="geojson" data={nycWifi}>
+        <Layer {...layerStyle} />
       </Source>
 
-      <Marker longitude={40.7554292155312} latitude={-73.98451127670837} color="red" /> */}
-    
+      <Marker>
+      longitude={longitude}
+      latitude={latitude}
+      <img src="../assets/pepe.png" />
+</Marker>
 
-//       </Map>
-    
-// </div>
-//   );
-// }
+    </Map>
+  );
+}
+
+export default Toilet;
 
 
-// export default Toilets
 
 
-// const Toilet = () => {
-//   const mapContainerRef = useRef(null);
 
-//   const [lng, setLng] = useState(5);
-//   const [lat, setLat] = useState(34);
-//   const [zoom, setZoom] = useState(1.5);
 
-//   // Initialize map when component mounts
-//   useEffect(() => {
-//     const map = new mapboxgl.Map({
-//       container: mapContainerRef.current,
-//       style: 'mapbox://styles/mapbox/streets-v11',
-//       center: [lng, lat],
-//       zoom: zoom
-//     });
 
-//     // Add navigation control (the +/- zoom buttons)
-//     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+// import React from 'react';
+// import Map, {Marker, Source, Layer} from "react-map-gl";
+// import "../styles/Toilets.css";
+// import nycWifi from "../assets/wifi.json";
 
-//     map.on('move', () => {
-//       setLng(map.getCenter().lng.toFixed(4));
-//       setLat(map.getCenter().lat.toFixed(4));
-//       setZoom(map.getZoom().toFixed(2));
-//     });
+// const MAPBOX_TOKEN = 'pk.eyJ1IjoiYmVja3l5eXl5IiwiYSI6ImNsZWV2azM0bTBiN2k0NG12cnEybml0am0ifQ.pTk0bhJgKynBeJMf1r8N3A'; // Set your mapbox token here
 
-//     // Clean up on unmount
-//     return () => map.remove();
-//   }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-//   return (
-//     <div>
-//       <div className='sidebarStyle'>
-//         <div>
-//           Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-//         </div>
-//       </div>
-//       <div className='map-container' ref={mapContainerRef} />
-//     </div>
-//   );
+// const layerStyle = {
+//   id: 'point',
+//   type: 'circle',
+//   paint: {
+//     'circle-radius': 10,
+//     'circle-color': '#007cbf'
+//   }
 // };
 
-// export default Toilet;
+
+
+
+// function App(){
+
+//   return (
+//       <div className="App">
+//         <Map
+//         mapboxAccessToken="pk.eyJ1IjoiYmVja3l5eXl5IiwiYSI6ImNsZWV2azM0bTBiN2k0NG12cnEybml0am0ifQ.pTk0bhJgKynBeJMf1r8N3A"
+//         style={{
+
+//               width:"90%",
+//               height: "60%",
+//               borderRadius:"15px",
+//               border: '2px solid red',
+//               top: "200px",
+//               bottom: "0",
+              
+                
+//         }}
+//         initialViewState={{
+//                   longitude: -73.98451127670837,
+//                   latitude: 40.7554292155312,
+//                   projection: 'globe',
+//                   zoom: 15,
+//                   pitch: 60
+//                 }}
+//         mapStyle="mapbox://styles/mapbox/streets-v12"
+//         >
+
+// <Source id="my-data" type="json" data={nycWifi}>
+//         <Layer {...layerStyle} />
+//       </Source>
+
+// <Marker 
+// longitude={-73.98451127670837}
+// latitude={40.7554292155312}
+        
+// />
+
+
+// </Map>
+// </div>
+// );
+// }
+
+// export default App
+
+
+
+
